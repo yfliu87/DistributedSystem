0311a81924d0aa234941dce0cb0b95fd9345e004
yifeiliu <yfliu87@gmail.com>
2021-03-22 21:14:31 -0700

    inital commit for leader election

---
+++ src/main/java/clusterCoordination/LeaderElection.java
@@ -0,0 +1,85 @@
+package clusterCoordination;
+
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooDefs;
+import org.apache.zookeeper.ZooKeeper;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+
+public class LeaderElection implements Watcher {
+    private static final String ZOOKEEPER_ADDRESS = "localhost:2181";
+    private static final int SESSION_TIMEOUT = 3000;
+    private static final String ELECTION_NAMESPACE = "/election";
+    private ZooKeeper zookeeper;
+    private String currentZnodeName;
+
+    public static void main(String[] args) throws IOException, InterruptedException,
+        KeeperException {
+        LeaderElection leaderElection = new LeaderElection();
+        leaderElection.connectToZookeeper();
+        leaderElection.volunteerForLeadership();
+        leaderElection.electLeader();
+        leaderElection.run();
+        leaderElection.close();
+
+        System.out.println("Disconnected from zookeeper, existing application...");
+    }
+
+    public void volunteerForLeadership() throws KeeperException, InterruptedException {
+        String znodePrefix = ELECTION_NAMESPACE + "/c_";
+        String znodeFullPath = zookeeper.create(znodePrefix, new byte[]{}, ZooDefs.Ids.OPEN_ACL_UNSAFE,
+                                                CreateMode.EPHEMERAL_SEQUENTIAL);
+
+        System.out.println("znode name " + znodeFullPath);
+        this.currentZnodeName = znodeFullPath.replace(ELECTION_NAMESPACE + "/", "");
+    }
+
+    public void electLeader() throws KeeperException, InterruptedException {
+        List<String> children = zookeeper.getChildren(ELECTION_NAMESPACE, false);
+        Collections.sort(children);
+
+        String smallestChild = children.get(0);
+
+        if (smallestChild.equals(currentZnodeName)) {
+            System.out.println("I am the leader");
+            return;
+        }
+
+        System.out.println("I am not the leader, " + smallestChild + " is the leader");
+    }
+
+    public void connectToZookeeper() throws IOException {
+        this.zookeeper = new ZooKeeper(ZOOKEEPER_ADDRESS, SESSION_TIMEOUT, this);
+    }
+
+    public void run() throws InterruptedException {
+        synchronized (zookeeper) {
+            zookeeper.wait();
+        }
+    }
+
+    public void close() throws InterruptedException {
+        zookeeper.close();
+    }
+
+    @Override
+    public void process(final WatchedEvent event) {
+        switch (event.getType()) {
+            case None:
+                if (event.getState() == Event.KeeperState.SyncConnected) {
+                    System.out.println("Successfully connected to zookeeper");
+                } else {
+                    synchronized (zookeeper) {
+                        System.out.println("Received disconnect from zookeeper event");
+                        zookeeper.notifyAll();
+                    }
+                }
+        }
+    }
+
+}
